02687676c7ec2b80640f5e08a704eddf21d535ab
Kamen Kanchev <45595147+skyllex94@users.noreply.github.com>
2024-02-15 15:10:18 +0200

    bottomsheet modal + context

--- App.js
+++ App.js
@@ -7,68 +7,87 @@ import SettingsScreen from "./components/tabs/Settings";
 // Icons and other UI
 import Ionicons from "@expo/vector-icons/Ionicons";
 import AddScreen from "./components/tabs/Add";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+// Context
+import { Context } from "./components/contexts/Global";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const bottomSheetModalRef = React.useRef();
+
+  const handlePresentModalPress = React.useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        options={{
-          tabBarOptions: {
-            style: {
-              backgroundColor: "white",
-            },
-          },
-        }}
-      >
-        <Tab.Screen
-          name="Home"
-          options={{
-            headerShown: false,
-            tabBarStyle: {
-              backgroundColor: "white",
-            },
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="home" size={24} color="black" />
-            ),
-          }}
-          component={HomeScreen}
-        />
+    <Context.Provider value={{ bottomSheetModalRef, handlePresentModalPress }}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <BottomSheetModalProvider>
+          <NavigationContainer>
+            <Tab.Navigator
+              options={{
+                tabBarOptions: {
+                  style: {
+                    backgroundColor: "white",
+                  },
+                },
+              }}
+            >
+              <Tab.Screen
+                name="Home"
+                options={{
+                  headerShown: false,
+                  tabBarStyle: {
+                    backgroundColor: "white",
+                  },
+                  tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="home" size={24} color="black" />
+                  ),
+                }}
+                component={HomeScreen}
+              />
 
-        <Tab.Screen
-          name="Add"
-          options={{
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <Ionicons name="add-sharp" size={24} color="black" />
-            ),
-          }}
-          component={AddScreen}
-        />
+              <Tab.Screen
+                name="Add"
+                options={{
+                  headerShown: false,
+                  tabBarIcon: ({ color }) => (
+                    <Ionicons name="add-sharp" size={24} color="black" />
+                  ),
+                }}
+                listeners={{
+                  tabPress: (e) => {},
+                }}
+                component={AddScreen}
+              />
 
-        <Tab.Screen
-          name="Templates"
-          options={{
-            headerShown: false,
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="newspaper" size={24} color="black" />
-            ),
-          }}
-          component={SettingsScreen}
-        />
+              <Tab.Screen
+                name="Templates"
+                options={{
+                  headerShown: false,
+                  tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="newspaper" size={24} color="black" />
+                  ),
+                }}
+                component={SettingsScreen}
+              />
 
-        <Tab.Screen
-          name="Settings"
-          options={{
-            headerShown: false,
-            tabBarIcon: () => (
-              <Ionicons name="settings" color="black" size={24} />
-            ),
-          }}
-          component={SettingsScreen}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+              <Tab.Screen
+                name="Settings"
+                options={{
+                  headerShown: false,
+                  tabBarIcon: () => (
+                    <Ionicons name="settings" color="black" size={24} />
+                  ),
+                }}
+                component={SettingsScreen}
+              />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
+    </Context.Provider>
   );
 }
--- components/tabs/Add.js
+++ components/tabs/Add.js
@@ -1,8 +1,7 @@
-import React, { useCallback, useMemo, useRef } from "react";
+import React, { useCallback, useContext, useMemo, useRef } from "react";
 import {
   View,
   Text,
-  StyleSheet,
   TouchableOpacity,
   Button,
   SafeAreaView,
@@ -10,43 +9,57 @@ import {
 // UI Elements
 import { AntDesign } from "@expo/vector-icons";
 // Dependencies
-import BottomSheet from "@gorhom/bottom-sheet";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { BottomSheetBackdrop, BottomSheetModal } from "@gorhom/bottom-sheet";
+import { Context } from "../contexts/Global";
 
 export default function AddScreen() {
-  const bottomSheetRef = useRef();
-  const snapPoints = useMemo(() => ["40%", "70%"], []);
+  const { bottomSheetModalRef, handlePresentModalPress } = useContext(Context);
+  const snapPoints = useMemo(() => ["50%"], []);
 
-  // Callbacks
   const handleSheetChanges = useCallback((index) => {
     console.log("handleSheetChanges", index);
   }, []);
 
   // renders
   return (
-    <GestureHandlerRootView className="flex-1">
-      <SafeAreaView className="flex-1 p-5, bg-white">
-        <Button
-          title="Open Sheet"
-          onPress={() => bottomSheetRef.current.expand()}
-        />
-        <BottomSheet
-          ref={bottomSheetRef}
-          index={0}
-          snapPoints={snapPoints}
-          onChange={handleSheetChanges}
-          enableOverDrag={true}
-          enablePanDownToClose={true}
-          animateOnMount={true}
-        >
-          <View className="flex-1 items-between">
-            <Text>Awesome 🎉</Text>
-            <TouchableOpacity onPress={() => bottomSheetRef.current.close()}>
-              <AntDesign name="close" size={24} color="black" />
-            </TouchableOpacity>
-          </View>
-        </BottomSheet>
-      </SafeAreaView>
-    </GestureHandlerRootView>
+    <SafeAreaView className="flex-1 p-5 bg-white">
+      <Button title="Open Sheet" onPress={handlePresentModalPress} />
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={0}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+        backdropComponent={(props) => (
+          <BottomSheetBackdrop
+            {...props}
+            appearsOnIndex={2}
+            disappearsOnIndex={-1}
+            enableTouchThrough={true}
+          />
+        )}
+        enableOverDrag={true}
+        enablePanDownToClose={true}
+        animateOnMount={true}
+      >
+        <View className="flex-row items-center justify-between px-3 pb-6">
+          <Text className="text-lg font-semibold">E-Sign Document</Text>
+          <TouchableOpacity
+            className="bg-gray-200 rounded-full p-2"
+            onPress={() => bottomSheetModalRef.current.close()}
+          >
+            <AntDesign name="close" size={20} color="black" />
+          </TouchableOpacity>
+        </View>
+
+        <View className="flex-row px-3 h-16">
+          <TouchableOpacity className="flex-1 bg-gray-200 rounded-md mr-3 p-2">
+            <Text>Draw</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="flex-1 bg-gray-200 rounded-md p-2">
+            <Text>Import Photo</Text>
+          </TouchableOpacity>
+        </View>
+      </BottomSheetModal>
+    </SafeAreaView>
   );
 }
